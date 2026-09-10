e6dad7d3360e096b4e548a33978bf97da6b78a1d
dodo-md <dodoznq+github@gmail.com>
2026-09-10 03:17:04 +0300

    merge app.py to mpv and radiobrowser

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from cofii!")
+from stations.radiobrowser import search_lofi
+from player.mpv import Player
 
+def main():
+    stations = search_lofi(10)
+    for i, s in enumerate(stations, 1):
+        print(f"{i}. {s['name']}")
+
+    choice = int(input("Seç: ")) - 1
+    url = stations[choice]["url_resolved"]
+
+    player = Player()
+    player.play(url)
+    input("Çalıyor… durdurmak için Enter")
+    player.stop()
 
 if __name__ == "__main__":
     main()
